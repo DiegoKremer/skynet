8402f1f1ca63eb290e70999bfe3504b9de8f3c54
Diego Kremer <diego.kremer@gmail.com>
2017-06-22 20:52:33 -0300

    Treina e avalia o dataset
    Adicionado os métodos que treina o modelo e avalia a precisão.

--- classificador_iris.py
+++ classificador_iris.py
@@ -47,8 +47,45 @@ test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
 #Especifica que todos os atributos são valores numéricos reais.
 feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
 
-#Constroi 3 camadas DNN com 10, 20 e 10 unidades respectivamente.
+#Constroi 3 camadas DNN com 10, 20 e 10 unidades respectivamente, sendo que os parametros
+# da classe DNNClassifier são feature_columns que é as colunas de atributos que definimos acima,
+# hidden_units são "camadas escondidas" usadas para melhorar performance do modelo, n_classses
+# que define o número de espécies de Iris que temos e por fim o caminho do diretório onde o
+# tensorflow irá salvar arquivos de checkpoints.
 classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                             hidden_units=[10,20,10],
                                             n_classes=3,
                                             model_dir="/tmp/iris_model")
+
+
+# Define a função de entrada dos dados de treino do modelo.
+def get_train_inputs():
+  x = tf.constant(training_set.data)
+  y = tf.constant(training_set.target)
+
+  return x, y
+
+
+# Juntamos o classificador que configuramos mais acima com os dados de treino e indicamos quantos
+# passos (steps) de treino ele irá realizar.
+
+#Encaixa o modelo no dataset.
+classifier.fit(input_fn=get_train_inputs, steps=2000)
+
+
+#Agora que o modelo de treino e os dados de treino estão prontos vamos usar o método evaluate
+# para avaliar a precisão
+
+#Define a função de entrada dos dados de teste do modelo.
+def get_test_inputs():
+  x = tf.constant(test_set.data)
+  y = tf.constant(test_set.target)
+
+
+#Avalia a precisão e printa na tela. Obs: O valor pode variar um pouco a cada treino realizado.
+accuracy_score = classifier.evaluate(input_fn=get_test_inputs,
+                                     steps=1)["accuracy"]
+
+print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
+
+
