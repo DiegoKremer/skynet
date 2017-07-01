592b7f77cb5305d8c8eabb85959c413db3e1df25
Diego Kremer <diego.kremer@gmail.com>
2017-07-01 13:57:19 -0300

    Concluido previsor de sobrevivencia Titanic
    Adicionado as entradas de dados, o processamento e exibicao.

--- exemplos_ml/titanic_sobrevive.py
+++ exemplos_ml/titanic_sobrevive.py
@@ -14,7 +14,7 @@ dados, labels = load_csv('titanic_dataset.csv', target_column=0,
                         categorical_labels=True, n_classes=2)
 
 # Funcao de preprocessamento dos dados
-def preprocess(dados, colunas_a_ignorar):
+def preprocessa(dados, colunas_a_ignorar):
     # Organiza por ordem decrescente de ID e deleta colunas ignoradas
     for id in sorted(colunas_a_ignorar, reverse=True):
         [r.pop(id) for r in dados]
@@ -28,7 +28,7 @@ Ignorar_colunas=[1, 6]
 
 # Preprocessa os dados carregados para remover os campos desnecessarios
 # e transformar atributos categoricos em numericos
-dados = preprocess(dados, Ignorar_colunas)
+dados = preprocessa(dados, Ignorar_colunas)
 
 # Constroi a rede neural
 rede = tflearn.input_data(shape=[None, 6])
@@ -41,7 +41,35 @@ rede = tflearn.regression(rede)
 modelo = tflearn.DNN(rede)
 
 # Inicia o treinamento
-modelo.fit(dados, labels, n_epoch=10, batch_size=48, show_metric=True)
+modelo.fit(dados, labels, n_epoch=10, batch_size=16, show_metric=True)
+
+#Entradas de nova pessoa
+classe = raw_input('A qual classe voce pertence?')
+nome = raw_input('Qual seu nome?')
+sexo = raw_input('Qual seu sexo?')
+#Traduz o sexo de portugues para ingles para corresponder ao dataset
+if (sexo == 'Feminino'):
+    sexo = 'Female'
+else:
+    sexo='Male'
+idade = raw_input('Qual sua idade?')
+irmaos_esposo = raw_input('Quantos irmaos e/ou esposa(o) levou?')
+pais_filhos = raw_input('Quantos pais e/ou filhos levou?')
+passagem = 'N/A'
+valor_passagem = raw_input('Valor da passagem?')
+
+#Cria uma pessoa com os dados inseridos
+pessoa = [classe, nome, sexo, idade, irmaos_esposo, pais_filhos, passagem, valor_passagem]
+
+#Processa essa pessoa para seus dados estejam corretos
+pessoa = preprocessa(pessoa, Ignorar_colunas)
+
+#Realiza a previsao baseado na pessoa criada e processada
+previsao = modelo.predict(pessoa)
+
+#Exibe a previsao na tela
+print("Chance de sobrevivencia...", previsao[0][1])
+
 
 
 
